be0753244786c2c6fba1f190c89dbad8b349f664
srimeghala <93635725+srimeghala@users.noreply.github.com>
2021-11-10 05:58:50 -0600

    second commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<center>
+<b>
+<?php  
+$x = 1;
+ echo "\t Welcome , Current Date: " . date("Y-m-d") . "<br>";
+while($x <= 5) {
+  echo "The number is: $x <br>";
+  $x++;
+} 
+?>  
+</b>
+</center>
+</body>
+</html>
